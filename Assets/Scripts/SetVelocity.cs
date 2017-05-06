577ca4c735cfe2faf1d43725e3314a8bc9c924cf
Seiji Emery <seijiemery@gmail.com>
2017-05-06 13:08:24 -0700

    Added blood vessel, fog to test scene

---
+++ Assets/Scripts/SetVelocity.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetVelocity : MonoBehaviour {
+
+	public Vector3 initialVelocity;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Rigidbody> ().velocity = initialVelocity;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

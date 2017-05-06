348469b5ba5628398bab57dd0e3695c17191c666
Seiji Emery <seijiemery@gmail.com>
2017-05-05 22:48:33 -0700

    Setup player gun script + player prefab

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class Player : MonoBehaviour {
+	public PlayerGun gun;
 
 	// Use this for initialization
 	void Start () {
@@ -11,6 +12,8 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetButton ("Fire1")) {
+			gun.Fire ();
+		}
 	}
 }
---
+++ Assets/Scripts/PlayerGun.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGun : MonoBehaviour {
+
+	public GameObject projectile;
+	public double     fireDelay = 0.1;
+	public float      fireVelocity = 1.0f;
+	private double    fireTimer = 0;
+	private uint 	  queuedProjectiles = 0;
+
+	// Fires a projectile from this gun transform.
+	// If cannot currently fire, queues a fire action.
+	public void Fire () {
+		MaybeFire ();
+	}
+	private bool MaybeFire () {
+		if (fireTimer <= 0) {
+			GameObject instance = Instantiate (projectile, transform.position, transform.rotation);
+			instance.GetComponent<Rigidbody> ().AddForce (transform.rotation * Vector3.forward * fireVelocity); 
+			fireTimer = fireDelay;
+			return true;
+		}
+		return false;
+	}
+	public void Update () {
+		fireTimer -= Time.deltaTime;
+
+		// Should we queue projectiles?
+		if (queuedProjectiles > 0 && MaybeFire()) {
+			queuedProjectiles -= 1;
+		}
+	}
+}

e254829d9c230d3036cdd647a55f5b85009d778d
Seiji Emery <seijiemery@gmail.com>
2017-05-06 10:13:11 -0700

    Added DeathTrigger script (now have 2 methods for cleaning up objects; deathtrigger can limit to tags or kill everything)

---
+++ Assets/Scripts/DeathTrigger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathTrigger : MonoBehaviour {
+
+	const bool killEverything = true;
+	public string[] killTags;
+
+	public void OnTriggerEnter (Collider other) {
+		if (killEverything) {
+			Destroy (other.gameObject);
+		} else {
+			foreach (string tag in killTags) {
+				if (other.gameObject.CompareTag (tag)) {
+					Destroy (other.gameObject);
+				}
+			}
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

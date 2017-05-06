15295ca6c0c6b803a9aa789a8b987e750c4d3a7e
Seiji Emery <seijiemery@gmail.com>
2017-05-06 11:02:21 -0700

    Added cell motion (rigidbody force – temp)

---
+++ Assets/Scripts/FluidMotion.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FluidMotion : MonoBehaviour {
+
+	public float force = 10.0f;
+
+	// Update is called once per frame
+	void Update () {
+		GetComponent<Rigidbody>().AddForce (Vector3.back * force * Time.deltaTime);
+	}
+}

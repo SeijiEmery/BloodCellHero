12cb80a07a54a2f36e0149e096ab2a5676283c93
Seiji Emery <seijiemery@gmail.com>
2017-05-06 21:15:16 -0700

    added repeating vein

---
+++ Assets/Scripts/LinearRepositioner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinearRepositioner : MonoBehaviour {
+	public double  velocity;
+//	public string targetTag = "Vein";
+	public double segmentLength = 120;
+	public int    numSegments   = 1;
+//	void ResetPosition () {
+//		transform.position.z = (float)(((double)transform.position.z) % (segmentLength * numSegments));
+//	}
+	void Update () {
+		// Issue / limitation: operations done relative to origin
+//		float truncOffset = (float)(segmentLength * numSegments);
+		double zcoord = (double)transform.position.z + velocity * Time.deltaTime;
+		double z1     = zcoord - segmentLength * numSegments;
+		Debug.Log (z1);
+		transform.position = new Vector3 (transform.position.x, transform.position.y, z1 < 0 ? (float)zcoord : (float)z1);
+//		if ((transform.position.z -= velocity * Time.deltaTime) >= truncOffset) {
+//			transform.position.z += truncOffset;
+//		}
+	}
+}

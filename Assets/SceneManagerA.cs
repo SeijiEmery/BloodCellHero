8b0e044b4944d8a2182ab2880de37f8b076b7927
Stephanie Andrews <stephanie@brilliant.org>
2017-05-07 13:14:43 -0700

    Modified non-MainScenes

---
+++ Assets/SceneManagerA.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagerA : MonoBehaviour {
+	protected float sceneStartTime;
+	public float waitTimeSeconds = 3.0f;
+	public string nextSceneToLoadStr;
+
+	// Use this for initialization
+	void Start () {
+		sceneStartTime = (float)Time.time;
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if ((float)(Time.time - sceneStartTime) > (waitTimeSeconds)) {
+			Debug.Log ("GOT THERE");
+			SceneManager.LoadScene (nextSceneToLoadStr);
+			Debug.Log (nextSceneToLoadStr);
+		}
+		Debug.Log (sceneStartTime);
+	}
+}
